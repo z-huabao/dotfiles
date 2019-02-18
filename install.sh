f2a385b3c204b6935056743e453ebe3574ed039b
zhonghuabao1 <329475907@qq.com>
2019-02-18 20:39:05 +0800

    update config files

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-cd ~
-
 paths="
     tmux/.tmux.conf
     tmux/.tmux
@@ -12,22 +10,26 @@ paths="
     zsh/antigen.zsh
 "
 
+cd ~
+
 for path in $paths
 do
     ln -s -f ~/ubuntu-config/$path
 done
 
+# config tmux, zsh, vim
 sudo apt install tmux vim zsh antigen trash-cli
-
-sudo apt install python3-pip
-sudo pip3 install xkeysnail
-# add `nohup bash ~/ubuntu-config/others/run-xkey.sh 'password' > /tmp/run-xkey.out &`
-# to gnome-session-properties to auto start xkey
-
 # use zsh as default shell
 chsh -s /bin/zsh
 
-# echo "replace bash to zsh in /etc/passwd" && sudo vim /etc/passwd
+
+# config xkeysnail to autostart
+sudo apt install python3-pip
+sudo pip3 install xkeysnail
+
+echo Please change username and password in '~/.config/autostart/'
+cp ~/ubuntu-config/others/xkey.desktop ~/.config/autostart/
+vi ~/.config/autostart/xkey.desktop
 
 
 echo -e "\nFinish!\n"
--- others/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-nohup bash ~/ubuntu-config/others/run-xkey.sh 0 > /tmp/run-xkey.out &
